87f6c938546b838c87b7ec7718edafd662541b0b
novacore system technology plc <novacoreplc@gmail.com>
2026-07-02 04:15:13 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'station_management',
-  waitForConnections: true,
-  connectionLimit: 10
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = pool;
+module.exports = pool.promise();
